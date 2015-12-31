a6c02c17964364c111146431eada10f56bfaf192
Jason Hitesman <jhitesma@gmail.com>
2015-12-30 21:46:37 -0700

    Changed configuration

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -160,10 +160,6 @@
 
 // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 // Ultimaker
-//    #define  DEFAULT_Kp 22.2
-//    #define  DEFAULT_Ki 1.08
-//    #define  DEFAULT_Kd 114
-
     #define  DEFAULT_Kp 20.93
     #define  DEFAULT_Ki 1.32
     #define  DEFAULT_Kd 82.71
@@ -203,9 +199,6 @@
 #ifdef PIDTEMPBED
 //120v 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
 //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-//    #define  DEFAULT_bedKp 10.00
-//    #define  DEFAULT_bedKi .023
-//    #define  DEFAULT_bedKd 305.4
     #define  DEFAULT_bedKp 209.88
     #define  DEFAULT_bedKi 13.01
     #define  DEFAULT_bedKd 846.80
@@ -325,7 +318,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 #define DISABLE_E false // For all extruders
 #define DISABLE_INACTIVE_EXTRUDER true //disable only inactive extruders and keep active extruder enabled
 
-#define INVERT_X_DIR false    // for Mendel set to false, for Orca set to true
+#define INVERT_X_DIR true    // for Mendel set to false, for Orca set to true
 #define INVERT_Y_DIR true    // for Mendel set to true, for Orca set to false
 #define INVERT_Z_DIR true     // for Mendel set to false, for Orca set to true
 #define INVERT_E0_DIR false   // for direct drive extruder v9 set to true, for geared extruder set to false
@@ -342,11 +335,11 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 #define max_software_endstops true  // If true, axis won't move to coordinates greater than the defined lengths below.
 
 // Travel limits after homing
-#define X_MAX_POS 195
+#define X_MAX_POS 200
 #define X_MIN_POS 0
-#define Y_MAX_POS 195
+#define Y_MAX_POS 200
 #define Y_MIN_POS 0
-#define Z_MAX_POS 125
+#define Z_MAX_POS 175
 #define Z_MIN_POS 0
 
 #define X_MAX_LENGTH (X_MAX_POS - X_MIN_POS)
